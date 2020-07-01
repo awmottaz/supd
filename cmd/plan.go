fba968c5ef291179d9378edc24a737cab03ae874
Tony Mottaz <tony@mottaz.dev>
2020-06-30 19:22:59 -0500

    set plan command

--- cmd/plan.go
+++ cmd/plan.go
@@ -23,93 +23,60 @@ THE SOFTWARE.
 package cmd
 
 import (
-	"fmt"
 	"os"
 
-	"github.com/awmottaz/supd/internal/prompt"
 	"github.com/awmottaz/supd/internal/update"
 	"github.com/spf13/cobra"
 )
 
-var listPlan bool
-var removePlan bool
+var force bool
 
 // planCmd represents the plan command
 var planCmd = &cobra.Command{
 	Use:   "plan",
-	Short: "set your plan for today",
-	Long:  `Use 'plan' to set or view your plan for today.`,
-	Args: func(cmd *cobra.Command, args []string) error {
-		if !listPlan && !removePlan && len(args) != 1 {
-			return fmt.Errorf("exactly 1 argument expected, received %d", len(args))
-		}
-		return nil
-	},
-	Run: runPlan,
-}
-
-func init() {
-	rootCmd.AddCommand(planCmd)
-
-	planCmd.Flags().BoolVarP(&listPlan, "list", "l", false, "list your plan for today")
-	planCmd.Flags().BoolVarP(&removePlan, "remove", "r", false, "remove your plan for today")
-}
-
-func runPlan(cmd *cobra.Command, args []string) {
-	filename, err := update.GetUpdatesFile()
-	if err != nil {
-		cmd.PrintErrln(err)
-		os.Exit(1)
-	}
-
-	collection := &update.Collection{}
-
-	err = collection.LoadFrom(filename)
-	if err != nil {
-		cmd.PrintErrln(err)
-		os.Exit(1)
-	}
-
-	upd, err := collection.FindByDate(update.Today())
-	if err != nil && err != update.NotFound {
-		cmd.PrintErrln("failed to read today's plan:", err)
-		os.Exit(1)
-	}
-	updFound := err != update.NotFound
-
-	if listPlan {
-		if !updFound {
-			cmd.Println()
-			return
-		}
-
-		cmd.Println(upd.Plan)
-		return
-	}
-
-	if removePlan {
-		cmd.Println("I cannot remove the plan, yet :(")
-		return
-	}
-
-	// If an update already exists for today, get user's permission to overwrite.
-	if updFound {
-		conf, err := prompt.Confirm(fmt.Sprintf("Plan for today already set to:\n\"%s\"\nOverwrite?", upd.Plan), false)
+	Short: "Set your plan",
+	Long:  `Set your plan`,
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		filename, err := update.GetUpdatesFile()
 		if err != nil {
 			cmd.PrintErrln(err)
 			os.Exit(1)
 		}
-		if !conf {
+
+		collection := &update.Collection{}
+
+		err = collection.LoadFrom(filename)
+		if err != nil {
+			cmd.PrintErrln(err)
 			os.Exit(1)
 		}
-	}
 
-	collection.Add(update.Update{Date: update.Today(), Plan: args[0]})
-	err = collection.Commit(filename)
-	if err != nil {
-		cmd.PrintErrln("failed to commit update:", err)
-		os.Exit(1)
-	}
+		_, err = collection.FindByDate(update.Today())
+		if err != nil && err != update.NotFound {
+			cmd.PrintErrln("failed to read today's plan:", err)
+			os.Exit(1)
+		}
 
-	cmd.Println("plan written to", filename)
+		if err != update.NotFound && !force {
+			cmd.PrintErrln("Plan already exists. Use '--force' to overwrite.")
+			os.Exit(1)
+		}
+
+		collection.Add(update.Update{Date: update.Today(), Plan: args[0]})
+
+		err = collection.Commit(filename)
+		if err != nil {
+			cmd.PrintErrln("failed to commit update:", err)
+			os.Exit(1)
+		}
+
+		cmd.Println("plan saved for", update.Today())
+	},
+}
+
+func init() {
+	setCmd.AddCommand(planCmd)
+
+	planCmd.Flags().BoolVarP(&force, "force", "f", false, "force overwrite a plan")
 }
